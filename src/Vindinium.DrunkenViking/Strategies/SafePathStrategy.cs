303ec79635ba6d47a7216a9a6e1e898493c78839
Corniel <mail@corniel.nl>
2014-10-20 08:07:42 +0200

    SafePath part 2...

---
+++ src/Vindinium.DrunkenViking/Strategies/SafePathStrategy.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Vindinium.DrunkenViking.Strategies
+{
+	public class SafePathStrategy : Strategy
+	{
+		public SafePathStrategy(Map map) : base(map) { }
+
+		public SafePathCollection Collection { get; protected set; }
+
+		public override bool Applies(State state, Tile location, Hero hero, PlayerType playerToMove)
+		{
+			this.Collection = SafePathCollection.Create(this.Map, state);
+			this.Collection.Procces();
+			return this.Collection.BestPath != null;
+		}
+
+		public override MoveDirection GetMove(State state, Tile location, Hero hero, PlayerType playerToMove)
+		{
+			return this.Collection.BestMove;
+		}
+	}
+}
