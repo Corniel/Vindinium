64bea4400484b77414baf13530515dcdab76b27c
Corniel <mail@corniel.nl>
2014-10-24 17:03:09 +0200

    Extensions on the drink beer processor.

---
+++ src/Vindinium/TileExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vindinium
+{
+	public static class TileExtensions
+	{
+		public static IEnumerable<Tile> GetPassables(this IEnumerable<Tile> tiles)
+		{
+			return tiles.Where(n => n.IsPassable);
+		}
+	}
+}
