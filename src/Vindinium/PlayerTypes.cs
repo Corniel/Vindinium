fa41e90147cf1d26156342fb58d31c03e7b0a375
Corniel <mail@corniel.nl>
2014-10-29 21:35:21 +0100

    Extended PlayerTypes with IEnumerable<PlayerType>.

--- src/Vindinium/PlayerTypes.cs
+++ src/Vindinium/PlayerTypes.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 
 namespace Vindinium
 {
 	/// <summary>Extra move players.</summary>
 	[DebuggerDisplay("{DebuggerDisplay}")]
-	public struct PlayerTypes
+	public struct PlayerTypes : IEnumerable<PlayerType>
 	{
 		/// <summary>Gets all player types.</summary>
 		public static PlayerType[] All = new PlayerType[] { PlayerType.Hero1, PlayerType.Hero2, PlayerType.Hero3, PlayerType.Hero4 };
@@ -22,7 +24,6 @@ namespace Vindinium
 		};
 
 		public static readonly PlayerTypes None = default(PlayerTypes);
-
 		public static readonly PlayerTypes Hero1 = new PlayerTypes(PlayerType.Hero1);
 		public static readonly PlayerTypes Hero2 = new PlayerTypes(PlayerType.Hero2);
 		public static readonly PlayerTypes Hero3 = new PlayerTypes(PlayerType.Hero3);
@@ -43,18 +44,27 @@ namespace Vindinium
 
 		public bool IsEmpty() { return m_Value == default(byte); }
 
-		public bool Contains(PlayerType direction)
+		/// <summary>Adds a player to the players. </summary>
+		public PlayerTypes Add(PlayerType player)
 		{
-			return (m_Value & (1 << (int)direction)) != 0;
+			var val = m_Value;
+			val |= (byte)(1 << (int)player);
+			return new PlayerTypes() { m_Value = val };
+		}
+		/// <summary>Removes a player from the players. </summary>
+		public PlayerTypes Remove(PlayerType player)
+		{
+			var val = m_Value;
+			val ^= (byte)(1 << (int)player);
+			return new PlayerTypes() { m_Value = val };
 		}
 
-		public PlayerType[] ToArray()
+		public bool Contains(PlayerType player)
 		{
-			var copy = this;
-			return All
-				.Where(direction => copy.Contains(direction))
-				.ToArray();
+			return (m_Value & (1 << (int)player)) != 0;
 		}
+
+		public PlayerType[] ToArray() { return GetItems().ToArray(); }
 
 		public override string ToString() { return String.Join("|", ToArray()); }
 
@@ -66,11 +76,24 @@ namespace Vindinium
 		private static byte ToByte(IEnumerable<PlayerType> players)
 		{
 			byte val = 0;
-			foreach (var direction in players)
+			foreach (var player in players.Where(p => p != PlayerType.None))
 			{
-				val |= (byte)(1 << (int)direction);
+				val |= (byte)(1 << (int)player);
 			}
 			return val;
+		}
+
+		public IEnumerator<PlayerType> GetEnumerator() { return GetItems().GetEnumerator(); }
+
+		[ExcludeFromCodeCoverage]
+		IEnumerator IEnumerable.GetEnumerator() { return GetItems().GetEnumerator(); }
+
+		private IEnumerable<PlayerType> GetItems()
+		{
+			foreach (var tp in All)
+			{
+				if (Contains(tp)) { yield return tp; }
+			}
 		}
 	}
 }
