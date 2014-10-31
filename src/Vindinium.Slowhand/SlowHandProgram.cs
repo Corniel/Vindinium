389a1af4608e9bea4432c256c97be80b720a88f0
Corniel <mail@corniel.nl>
2014-10-31 19:13:06 +0100

    GUI update (input in background worker)

--- src/Vindinium.Slowhand/SlowHandProgram.cs
+++ src/Vindinium.Slowhand/SlowHandProgram.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Configuration;
 using System.Diagnostics;
+using System.Threading;
 using Vindinium.App;
 using Vindinium.Viewer;
 
@@ -16,11 +17,29 @@ namespace Vindinium.SlowHand
 		public SlowHandProgram()
 		{
 			this.Timout = TimeSpan.FromMilliseconds(int.Parse(ConfigurationManager.AppSettings["maxtime"]));
+
+			ThreadPool.QueueUserWorkItem((waitpoint) =>
+			{
+				while (true)
+				{
+					var key = Console.ReadKey();
+					switch (key.Key)
+					{
+						case ConsoleKey.LeftArrow: this.Move = MoveDirection.W; this.ReceivedMove = false; break;
+						case ConsoleKey.RightArrow: this.Move = MoveDirection.E; this.ReceivedMove = false; break;
+						case ConsoleKey.UpArrow: this.Move = MoveDirection.N; this.ReceivedMove = false; break;
+						case ConsoleKey.DownArrow: this.Move = MoveDirection.S; this.ReceivedMove = false; break;
+						case ConsoleKey.X: this.Move = MoveDirection.x; this.ReceivedMove = false; break;
+						default: break;
+					}
+				}
+			});
 		}
 
 		public TimeSpan Timout { get; protected set; }
 
 		public MoveDirection Move { get; protected set; }
+		public bool ReceivedMove { get; protected set; }
 
 		
 		public override Move GetMove()
@@ -31,33 +50,12 @@ namespace Vindinium.SlowHand
 
 			Render();
 
-
-			ConsoleKeyInfo key = new ConsoleKeyInfo();
-
-			while (sw.Elapsed < this.Timout)
+			while (!this.ReceivedMove && sw.Elapsed < this.Timout)
 			{
-				
-				if (Console.KeyAvailable == true)
-				{
-					key = Console.ReadKey();
-					break;
-				}
-				else
-				{
-					Console.Write("\r{0} {1:##0.0}", this.Move, (this.Timout - sw.Elapsed).TotalMilliseconds);
-					System.Threading.Thread.Sleep(170);
-				}
-				
+				Console.Write("\r{0} {1:0.0}", this.Move, (this.Timout - sw.Elapsed).TotalMilliseconds/1000.0);
+				System.Threading.Thread.Sleep(17);
 			}
-			switch (key.Key)
-			{
-				case ConsoleKey.LeftArrow: this.Move = MoveDirection.W;  break;
-				case ConsoleKey.RightArrow: this.Move = MoveDirection.E; break;
-				case ConsoleKey.UpArrow: this.Move = MoveDirection.N; break;
-				case ConsoleKey.DownArrow: this.Move = MoveDirection.S; break;
-				case ConsoleKey.X: this.Move = MoveDirection.x; break;
-				default: break;
-			}
+			this.ReceivedMove = false;
 			try
 			{
 				this.State = this.State.Move(this.Map, this.Move, this.Player);
